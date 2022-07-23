aa21dc55f379b0c7490e3d76ca98e852e9fd7635
Anatoly Fomin <58336508+Fomin-Anatolii@users.noreply.github.com>
2022-07-23 20:16:49 +0800

    added input box

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      monsters: [],
+      searchField: "",
+    }
+  }
+
+  componentDidMount() {
+    fetch(`https://jsonplaceholder.typicode.com/users
+    `)
+      .then((promise) => promise.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users }
+        })
+      )
+  }
+
+  onSearchChange = (event) => {
+    const searchFiled = event.target.value.toLocaleLowerCase()
+    this.setState(() => {
+      return { searchFiled }
+    })
+  }
+
+  render() {
+    const { monsters, searchField } = this.state
+    const { onSearchChange } = this
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    })
+
+    return (
+      <div className="App">
+        <input
+          type="search"
+          className="search-box"
+          placeholder="search monsters"
+          onChange={onSearchChange}
+        />
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
